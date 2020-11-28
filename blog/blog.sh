5efb79234464a0a434dbf7229906e455f79290d5
Wojciech Kwolek <wojciech@kwolek.xyz>
2020-11-28 22:50:03 +0100

    support Gemini feed format + reformat paragraphs to be on single lines as per the spec

--- blog/blog.sh
+++ blog/blog.sh
@@ -3,21 +3,26 @@ title() {
     head -n1 "$1" | sed -r 's/^\#+\s+//'
 }
 
+date() {
+    echo "$(basename "$1")" | cut -d_ -f1
+}
+
 new() {
     cp template "posts/$1.gmi"
     nvim "posts/$1.gmi"
 }
 
 generate() {
-    echo "# Wojciech ~irth Kwolek" > index.gmi
+    echo "# Wojciech ~irth Kwolek's blag" > index.gmi
     echo "=> .. Back" >> index.gmi
     echo >> index.gmi
-    echo "## Blog" >> index.gmi
-    echo "" >> index.gmi
 
     for i in $(ls posts/*.gmi | sort -nr); do
-        echo "=> $i  $(title "$i")" >> index.gmi
+        echo "=> $i $(date "$i") - $(title "$i")" >> index.gmi
     done
+
+    echo "" >> index.gmi
+    echo "This can be reached over gemini:// at the same path." >> index.gmi
 }
 
 "$@"
